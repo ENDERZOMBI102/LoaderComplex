6679407b18849dda46e7b0dd9e2de930bcb048a7
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-02-22 11:43:49 +0100

    yes
    Signed-off-by: ENDERZOMBI102 <enderzombi102.end@gmail.com>

--- loadercomplex-common/src/main/java/com/enderzombi102/loadercomplex/common/abstraction/block/Block.java
+++ loadercomplex-common/src/main/java/com/enderzombi102/loadercomplex/common/abstraction/block/Block.java
@@ -1,8 +1,9 @@
 package com.enderzombi102.loadercomplex.common.abstraction.block;
 
-import com.enderzombi102.loadercomplex.common.abstraction.utils.Identifer;
+import com.enderzombi102.loadercomplex.common.abstraction.utils.CIdentifier;
 
 public interface Block {
 	BlockMaterial getMaterial();
-	void setIdentifier(Identifer id);
+	void setIdentifier(CIdentifier id);
+	void setIdentifier(String id);
 }
--- loadercomplex-common/src/main/java/com/enderzombi102/loadercomplex/common/abstraction/item/Item.java
+++ loadercomplex-common/src/main/java/com/enderzombi102/loadercomplex/common/abstraction/item/Item.java
@@ -1,4 +1,8 @@
 package com.enderzombi102.loadercomplex.common.abstraction.item;
 
+import com.enderzombi102.loadercomplex.common.abstraction.utils.CIdentifier;
+
 public interface Item {
+	void setIdentifier(CIdentifier item);
+	void setIdentifier(String id);
 }
