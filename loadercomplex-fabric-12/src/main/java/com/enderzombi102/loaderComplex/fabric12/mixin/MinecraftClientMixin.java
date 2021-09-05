6332cc064d2867270f40a879729146d8af1d4382
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-09-06 00:04:03 +0200

    trying to fix RPs

---
+++ loadercomplex-fabric-12/src/main/java/com/enderzombi102/loaderComplex/fabric12/mixin/MinecraftClientMixin.java
@@ -0,0 +1,23 @@
+package com.enderzombi102.loaderComplex.fabric12.mixin;
+
+import com.enderzombi102.loaderComplex.fabric12.LoaderComplexFabric;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.resource.ResourcePack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.List;
+
+@Mixin(MinecraftClient.class)
+public abstract class MinecraftClientMixin {
+
+	@Inject(method = "reloadResources", at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/ReloadableResourceManager;reload(Ljava/util/List;)V", ordinal = 0), locals = LocalCapture.CAPTURE_FAILHARD)
+	public void onReloadResources(CallbackInfo ci, List<ResourcePack> list) {
+		list.addAll(LoaderComplexFabric.packs);
+	}
+
+
+}
