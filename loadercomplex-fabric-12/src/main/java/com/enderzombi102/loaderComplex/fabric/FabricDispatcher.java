09e42c584ca8c16f73ad227e986b11d6542f99bc
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-04-02 20:22:13 +0200

    implement a fabric dispatcher for when we'll have more than one fabric version

---
+++ loadercomplex-fabric-12/src/main/java/com/enderzombi102/loaderComplex/fabric/FabricDispatcher.java
@@ -0,0 +1,26 @@
+package com.enderzombi102.loaderComplex.fabric;
+
+import com.enderzombi102.loaderComplex.fabric12.LoaderComplexFabric;
+import com.enderzombi102.loadercomplex.Utils;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.impl.FabricLoaderImpl;
+
+public class FabricDispatcher implements ModInitializer {
+	private final ModInitializer impl;
+
+	public FabricDispatcher() {
+		String ver = FabricLoaderImpl.INSTANCE.getGameProvider().getNormalizedGameVersion();
+		switch ( ver ) {
+			case "1.12.2":
+				impl = new LoaderComplexFabric();
+				break;
+			default:
+				throw new IllegalStateException( Utils.format( "Fabric for {} is not supported!", ver ) );
+		}
+	}
+
+	@Override
+	public void onInitialize() {
+		this.impl.onInitialize();
+	}
+}
