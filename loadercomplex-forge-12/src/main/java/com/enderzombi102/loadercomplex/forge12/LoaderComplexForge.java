a3ed0f860b5991a211c803a553336773c01976dd
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-04-03 19:30:07 +0200

    trying to make addons show up in mods screen

--- loadercomplex-forge-12/src/main/java/com/enderzombi102/loadercomplex/forge12/LoaderComplexForge.java
+++ loadercomplex-forge-12/src/main/java/com/enderzombi102/loadercomplex/forge12/LoaderComplexForge.java
@@ -1,9 +1,11 @@
 package com.enderzombi102.loadercomplex.forge12;
 
 import com.enderzombi102.loadercomplex.LoaderComplex;
+import com.enderzombi102.loadercomplex.Utils;
 import com.enderzombi102.loadercomplex.forge12.impl.ForgeLoader;
+import com.google.common.collect.ImmutableList;
 import net.minecraftforge.fml.client.FMLClientHandler;
-import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.LoadController;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.ModContainer;
@@ -13,15 +15,16 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 @Mod(
-		modid = LoaderComplexForge.MOD_ID,
-		name = LoaderComplexForge.MOD_NAME,
-		version = LoaderComplexForge.VERSION,
-		acceptedMinecraftVersions =  "1.12.2",
-		dependencies = "required:forge"
+	modid = LoaderComplexForge.MOD_ID,
+	name = LoaderComplexForge.MOD_NAME,
+	version = LoaderComplexForge.VERSION,
+	acceptedMinecraftVersions =  "1.12.2",
+	dependencies = "required:forge"
 )
 public class LoaderComplexForge extends LoaderComplex {
 	public static final String MOD_ID = "loadercomplex";
@@ -45,19 +48,51 @@ public class LoaderComplexForge extends LoaderComplex {
 			// wrap AddonContainer in a forge-compatible one
 			FrogeModContainer frogeModContainer = new FrogeModContainer( container );
 			containers.add( frogeModContainer );
-			LOGGER.info( "Registering new forge mod container: " + container.getName() );
+			injectContainer( frogeModContainer );
 			FMLClientHandler.instance().addModAsResource( frogeModContainer );
 		};
 		this.container = Loader.instance().activeModContainer();
 		this.initAddons();
 	}
 
-	public ForgeLoader getLoader() {
-		return (ForgeLoader) this.loader;
+	public void injectContainer(FrogeModContainer frogeModContainer) {
+		LOGGER.info( Utils.format("Injecting ModContainer for \"{}\" into mods list!", frogeModContainer.getModId() ) );
+		Loader loader = Loader.instance();
+		// inject into modlist
+		try {
+			Field field = loader.getClass().getDeclaredField("mods");
+			field.setAccessible(true);
+			//noinspection unchecked
+			List<ModContainer> imods = (List<ModContainer>) field.get( loader );
+			if ( imods instanceof ImmutableList ) {
+				LOGGER.info("Mod list is immutable! making mutable...");
+				// make it mutable again
+				field.set( loader, new ArrayList<>( imods ) );
+				LOGGER.info("Mod list is now mutable!");
+			}
+			//noinspection unchecked
+			( (List<ModContainer>) field.get( loader ) ).add( frogeModContainer );
+		} catch (NoSuchFieldException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		LOGGER.info( "Container injected!" );
 	}
 
-	public List<FrogeModContainer> getContainers() {
-		return containers;
+	public void injectActiveContainer(FrogeModContainer frogeModContainer) {
+		LOGGER.info( Utils.format("Injecting ModContainer for \"{}\" into active mods list!", frogeModContainer.getModId() ) );
+		try {
+			Field field = loader.getClass().getDeclaredField("modController");
+			field.setAccessible(true);
+			LoadController controller = (LoadController) field.get( loader );
+			controller.getActiveModList().add( frogeModContainer );
+		} catch (NoSuchFieldException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		LOGGER.info( "Container injected!" );
+	}
+
+	public ForgeLoader getLoader() {
+		return (ForgeLoader) this.loader;
 	}
 
 	/**
@@ -65,9 +100,7 @@ public class LoaderComplexForge extends LoaderComplex {
 	 * The registry events below will have fired prior to entry to this method.
 	 */
 	@Mod.EventHandler
-	public void preinit(FMLPreInitializationEvent evt) {
-		// register custom resource packs
-	}
+	public void preinit(FMLPreInitializationEvent evt) { }
 
 	/**
 	 * This is the second initialization event. Register custom recipes
