edbb22418f6de0cc693f245c3194c040f57a57c1
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-04-09 20:49:14 +0200

    old fabric stuff

---
+++ loadercomplex-fabric-12/src/main/java/com/enderzombi102/loadercomplex/fabric12/compat/LoaderComplexModMenuEntrypoint.java
@@ -0,0 +1,18 @@
+package com.enderzombi102.loadercomplex.fabric12.compat;
+
+import com.enderzombi102.loadercomplex.fabric.FabricDispatcher;
+import com.enderzombi102.loadercomplex.fabric12.LoaderComplexFabric;
+import com.enderzombi102.loadercomplex.modloader.AddonContainer;
+import io.github.prospector.modmenu.ModMenu;
+import io.github.prospector.modmenu.api.ModMenuApi;
+
+public class LoaderComplexModMenuEntrypoint implements ModMenuApi {
+	static {
+		if ( FabricDispatcher.getMinecraftVersion().equals( "1.12.2" ) )
+			for ( AddonContainer container : LoaderComplexFabric.INSTANCE.getAddonLoader().getAddons() )
+				ModMenu.PARENT_MAP.put(
+					ModMenu.MODS.get("loadercomplex"),
+					ModMenu.MODS.put( container.getID(), new LoaderComplexModImpl( container ) )
+				);
+	}
+}
