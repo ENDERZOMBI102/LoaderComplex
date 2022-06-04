ecf6246207a88ff63bb4f52700dd93caaa140e54
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-06-04 23:11:09 +0200

    even more stuff

--- loadercomplex-common/src/main/java/com/enderzombi102/loadercomplex/api/adv/command/CommandContext.java
+++ loadercomplex-common/src/main/java/com/enderzombi102/loadercomplex/api/adv/command/CommandContext.java
@@ -1,7 +1,14 @@
 package com.enderzombi102.loadercomplex.api.adv.command;
 
+import com.enderzombi102.loadercomplex.api.entity.LivingEntity;
 import org.jetbrains.annotations.ApiStatus.AvailableSince;
+import org.jetbrains.annotations.Nullable;
 
 @AvailableSince( "0.2.0" )
-public class CommandContext {
+public interface CommandContext {
+	@Nullable LivingEntity getSender();
+	default void reply( String text ) {
+		if ( this.getSender() != null && this.getSender().isPlayer() )
+			this.getSender().asPlayer().sendMessage( text );
+	}
 }
