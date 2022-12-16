8722924b94789d33c9cfb0468593ce4f50b2b850
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-12-16 12:30:21 +0100

    re-add stuff deleted by accident

---
+++ loadercomplex-fabric/common/src/main/java/com/enderzombi102/loadercomplex/fabric/FabricDispatcher.java
@@ -0,0 +1,53 @@
+package com.enderzombi102.loadercomplex.fabric;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+
+/**
+ * Dispatcher for any fabric version
+ */
+public class FabricDispatcher implements ModInitializer {
+	private final ModInitializer impl;
+
+	public FabricDispatcher() {
+		try {
+			switch ( getMinecraftVersion() ) {
+				case "1.12.2":
+					impl = (ModInitializer) Class.forName("com.enderzombi102.loadercomplex.fabric12.LoaderComplexFabric")
+						.getConstructor()
+						.newInstance();
+					break;
+				case "1.17.1":
+					impl = (ModInitializer) Class.forName("com.enderzombi102.loadercomplex.fabric17.LoaderComplexFabric")
+						.getConstructor()
+						.newInstance();
+					break;
+				case "b1.7.3":
+					impl = (ModInitializer) Class.forName("com.enderzombi102.loadercomplex.fabric173.LoaderComplexFabric")
+						.getConstructor()
+						.newInstance();
+					break;
+				default:
+					throw new IllegalStateException( String.format( "Fabric for %s is not supported!", getMinecraftVersion() ) );
+			}
+		} catch (Exception e) {
+			throw new IllegalStateException( e );
+		}
+		System.out.println("Hello world!");
+	}
+
+	@Override
+	public void onInitialize() {
+		this.impl.onInitialize();
+	}
+
+	public static String getMinecraftVersion() {
+		//noinspection OptionalGetWithoutIsPresent
+		return FabricLoader.getInstance()
+			.getModContainer("minecraft")
+			.get()
+			.getMetadata()
+			.getVersion()
+			.getFriendlyString();
+	}
+}
