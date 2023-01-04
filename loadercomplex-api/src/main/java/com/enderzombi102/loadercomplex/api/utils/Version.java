00e45e7fabc005f95f1469c9a86498a717c8470c
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2023-01-04 12:02:21 +0100

    fix crash when trying to get the api version

--- loadercomplex-api/src/main/java/com/enderzombi102/loadercomplex/api/utils/Version.java
+++ loadercomplex-api/src/main/java/com/enderzombi102/loadercomplex/api/utils/Version.java
@@ -7,14 +7,8 @@ import org.jetbrains.annotations.NotNull;
  */
 public final class Version {
 	private final int major, minor, patch;
-	private final String buildDate;
-
-	public Version(int major, int minor, int patch, String buildDate) {
-		this.major = major;
-		this.minor = minor;
-		this.patch = patch;
-		this.buildDate = buildDate;
-	}
+	private final @NotNull String buildDate;
+	private final @NotNull String buildTime;
 
 	public Version( @NotNull String version, @NotNull String buildDate ) {
 		String[] parts = version.split("\\.");
@@ -23,7 +17,19 @@ public final class Version {
 		this.major = Integer.parseInt( parts[0] );
 		this.minor = Integer.parseInt( parts[1] );
 		this.patch = Integer.parseInt( parts[2] );
-		this.buildDate = buildDate.substring(0, buildDate.indexOf('T') );
+		this.buildDate = buildDate.substring( 0, buildDate.indexOf('T') );
+		this.buildTime = buildDate.substring( buildDate.indexOf('T') );
+	}
+
+	public Version( @NotNull String version, @NotNull String buildDate, @NotNull String buildTime ) {
+		String[] parts = version.split("\\.");
+		// we need all three parts of the version
+		assert parts.length == 3;
+		this.major = Integer.parseInt( parts[0] );
+		this.minor = Integer.parseInt( parts[1] );
+		this.patch = Integer.parseInt( parts[2] );
+		this.buildDate = buildDate;
+		this.buildTime = buildTime;
 	}
 
 	public int getMajor() {
@@ -38,21 +44,27 @@ public final class Version {
 		return patch;
 	}
 
-	public String getBuildDate() {
+	public @NotNull String getBuildDate() {
 		return buildDate;
 	}
 
-	public String asString() {
-		return major + "." + minor + "." + patch + "-build." + buildDate.replaceAll("-", "");
+	public @NotNull String getBuildTime() {
+		return buildTime;
+	}
+
+	public @NotNull String asString() {
+		return major + "." + minor + "." + patch + "-build." + buildDate.replaceAll( "-", "" );
 	}
 
 	@Override
 	public String toString() {
-		return "Version{" +
-				"major=" + major +
-				", minor=" + minor +
-				", patch=" + patch +
-				", buildDate='" + buildDate + '\'' +
-				'}';
+		return String.format(
+			"Version{major=%s, minor=%s, patch=%s, buildDate='%s', buildTime='%s'}",
+			major,
+			minor,
+			patch,
+			buildDate,
+			buildTime
+		);
 	}
 }
--- loadercomplex-api/src/main/java/com/enderzombi102/loadercomplex/impl/Utils.java
+++ loadercomplex-api/src/main/java/com/enderzombi102/loadercomplex/impl/Utils.java
@@ -2,12 +2,14 @@ package com.enderzombi102.loadercomplex.impl;
 
 import blue.endless.jankson.Jankson;
 import com.enderzombi102.loadercomplex.api.utils.Version;
+import org.jetbrains.annotations.Nullable;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
@@ -15,29 +17,42 @@ import java.util.jar.Manifest;
  * Some common utilities used in many parts of LoaderComplex
  */
 public final class Utils {
+	private static @Nullable Version apiVersion = null;
 	private Utils() { }
 
 	public static final Jankson JANKSON = Jankson.builder().build();
 
 	/**
 	 * Tries to get the current API version of this LC implementation
-	 * @param isDevEnv whether this is a developer environment
 	 * @return version of the currently implemented API
 	 * @throws IllegalStateException if the LoaderComplex's manifest is somehow broken
 	 */
-	public static Version getApiVersion( boolean isDevEnv ) {
-		if ( isDevEnv )
-			return new Version(
-				"1.0.0",
-				LocalDateTime.now().format( DateTimeFormatter.ofPattern("dd-MM-yyyy'T'HH:mm:ss") )
-			);
-		// FIXME: only works on prod?
-		try {
-			final Attributes fest = new Manifest( Utils.class.getResourceAsStream("/META-INF/MANIFEST.MF") ).getMainAttributes();
-			return new Version( fest.getValue( "Specification-Version" ), fest.getValue("Implementation-Timestamp") );
-		} catch (IOException ignored) {
-			throw new IllegalStateException("Failed to read manifest from LoaderComplex's jar");
+	public static Version getApiVersion() {
+		if ( apiVersion != null )
+			return apiVersion;
+
+		InputStream stream = Utils.class.getResourceAsStream( "/api-version" );
+		if ( stream != null ) {
+			try {
+				BufferedReader reader = new BufferedReader( new InputStreamReader( stream ) );
+				apiVersion = new Version( reader.readLine(), reader.readLine() );
+			} catch ( IOException e ) {
+				throw new IllegalStateException( "Failed to read version information from LoaderComplex's jar", e );
+			} finally {
+				try {
+					stream.close();
+				} catch ( IOException ignored ) { }
+			}
 		}
+		if ( apiVersion == null ) {
+			try {
+				Attributes attr = new Manifest( Utils.class.getResourceAsStream( "/META-INF/MANIFEST.MF" ) ).getMainAttributes();
+				apiVersion = new Version( attr.getValue( "Specification-Version" ), attr.getValue( "Implementation-Timestamp" ) );
+			} catch ( IOException e ) {
+				throw new IllegalStateException( "Failed to read version information from LoaderComplex's jar", e );
+			}
+		}
+		return apiVersion;
 	}
 
 	/**
