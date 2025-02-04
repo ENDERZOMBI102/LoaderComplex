88edbab4c02a4106f10ab71fed5593ea413bf211
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2025-02-04 02:12:40 +0100

    chore: Re-compile test addon jar when starting the game

--- buildSrc/src/main/kotlin/lc/Helper.kt
+++ buildSrc/src/main/kotlin/lc/Helper.kt
@@ -5,20 +5,21 @@ import xyz.wagyourtail.unimined.api.runs.RunsConfig
 
 
 fun RunsConfig.setupRuns() {
-	arrayOf( "client", "server" ).forEach { type ->
-		config( type ) {
+	arrayOf( "client", "server" ).forEach { side ->
+		config( side ) {
 			val version = name.substring( 3, 6 )
 				.chunked( 2 )
 				.joinToString( ".", "1." )
 			val loader = project.name.substring( 14 )
 
-			description = "$loader $version $type"
+			description = "$loader $version $side"
 
 			javaVersion = JavaVersion.VERSION_17
 
 			jvmArgs( "-Dlog4j.configurationFile=${project.rootProject.file("log4j2.xml").absolutePath}" )
 			jvmArgs( "-Dlog4j2.configurationFile=${project.rootProject.file("log4j2.xml").absolutePath}" )
-			workingDir( project.rootProject.file( "run/$type" ).absolutePath )
+			workingDir( project.rootProject.file( "run/$side" ).absolutePath )
 		}
+		preLaunch( side, ":loadercomplex-test:jar" )
 	}
 }
